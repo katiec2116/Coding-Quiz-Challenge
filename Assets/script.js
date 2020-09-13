52d41b88cf3026ae22bc6c216d1d391438cdbc78
katiec <katiecampbell21@outlook.com>
2020-09-13 09:34:49 -0400

    created start button and added in placeholder questions

---
+++ Assets/script.js
@@ -0,0 +1,25 @@
+var questions = [
+    {
+      title: "Example Question 1:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "Choice 1"
+    },
+    {
+      title: "Example Question 2:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "Choice 2"
+    },
+    {
+        title: "Example Question 1:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "Choice 3"
+      },
+      {
+        title: "Example Question 2:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "Choice 4"
+      }
+  ];
+
+
+  
